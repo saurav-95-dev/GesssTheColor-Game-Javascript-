ded3ce924865d6446b62be4b74e3f1b9f7304911
Saurabh <devsaurabh086@gmail.com>
2025-01-10 15:01:26 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let colorCode = document.getElementById("color-code");
 let optionContainer = document.getElementById("option-container");
+let scoreCount = document.getElementById("score");
 
 let randomColor = null;
-
+let score = 0;
 
 let generateRandomeNumberRGB = (min, max) => {
     return min + Math.floor(Math.random() * (max - min));
 }
-
 
 let generateRandomRGB = () => {
     let red = generateRandomeNumberRGB(0,255);
@@ -23,28 +23,46 @@ console.log(generateRandomeNumberRGB(0, 255));
 //Console display of rbg val:
 console.log(generateRandomRGB());
 
+//Increment score on correct selection:
+let incrementScore = () => {
+    score += 1;
+    scoreCount.innerText = score;
+}
+
+let validateResult = (e) => {
+    let selectedColor = e.target.style.backgroundColor.replaceAll(" ", "");
+    if (selectedColor === randomColor.replaceAll(" ", "")) {
+        incrementScore();
+    }
+    startGame();
+}
+
 
 //Start game function on reloading window ; 
 let startGame = () => {
+    optionContainer.innerHTML = null;
     randomColor = generateRandomRGB();
-    //Using option-container html class:
     colorCode.innerText = randomColor;
-    console.log(colorCode);
-    let presentIndex = generateRandomeNumberRGB();
-    console.log("Present index value ", presentIndex);
-    for (let i=0; i < 6; i++) {
+
+    let presentIndex = generateRandomeNumberRGB(0, 6); // Random index to insert the correct color
+    console.log("Present index value", presentIndex);
+
+    for (let i = 0; i < 6; i++) {
         let div = document.createElement("div");
+
+        div.addEventListener("click", validateResult);
         div.style.height = "60px";
         div.style.width = "60px";
         div.style.margin = "5px";
         div.style.borderRadius = "10px";
-        div.style.backgroundColor = generateRandomRGB();
+
         // Place `randomColor` in the `presentIndex` slot
         if (i === presentIndex) {
             div.style.backgroundColor = randomColor;
         } else {
             div.style.backgroundColor = generateRandomRGB();
         }
+
         optionContainer.append(div);
     }
 }
