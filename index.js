d26b980cdb3d56441c5fa0e4ccd1b3f4f68a6068
Saurabh <devsaurabh086@gmail.com>
2025-01-10 12:41:09 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let colorCodeContainer = document.getElementById("color-code");
+let colorCode = document.getElementById("color-code");
 let optionContainer = document.getElementById("option-container");
 
 let randomColor = null;
-let score = 0;
 
-// Generic function for generating random number between two numbers: 
+
 let generateRandomeNumberRGB = (min, max) => {
     return min + Math.floor(Math.random() * (max - min));
 }
@@ -18,38 +17,37 @@ let generateRandomRGB = () => {
 
 }
 
-let incrementScore = () => {
-    score += 1;
-}
-
 //Value of generateRandomNumberRGB:
 console.log(generateRandomeNumberRGB(0, 255));
 
 //Console display of rbg val:
 console.log(generateRandomRGB());
 
-let validateResult = (e) => {
-    console.log(e.target);
-    let selectedColor = e.target.style.backgroundColor;
-    console.log(selectedColor===randomColor);
-}
-//Start game function:
+
+//Start game function on reloading window ; 
 let startGame = () => {
     randomColor = generateRandomRGB();
-    colorCodeContainer.innerText = randomColor;
+    //Using option-container html class:
+    colorCode.innerText = randomColor;
+    console.log(colorCode);
     let presentIndex = generateRandomeNumberRGB();
+    console.log("Present index value ", presentIndex);
     for (let i=0; i < 6; i++) {
         let div = document.createElement("div");
-        div.addEventListener("click" , validateResult())
         div.style.height = "60px";
         div.style.width = "60px";
         div.style.margin = "5px";
         div.style.borderRadius = "10px";
-        div.style.backgroundColor =
-            i === presentIndex ? randomColor : generateRandomRGB();
+        div.style.backgroundColor = generateRandomRGB();
+        // Place `randomColor` in the `presentIndex` slot
+        if (i === presentIndex) {
+            div.style.backgroundColor = randomColor;
+        } else {
+            div.style.backgroundColor = generateRandomRGB();
+        }
         optionContainer.append(div);
     }
 }
 
-//Reloading window to generate new color everytime !
+//Reloading window to generate new color everytime by calling startGame function !
 window.addEventListener("load", startGame());
